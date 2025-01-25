93fca322026aa8694ae2d04bca62eba8214df944
Kira <gajdukkira232@gmail.com>
2025-01-25 20:56:45 +0200

    upadate

---
+++ js/Homework1.js
@@ -0,0 +1,42 @@
+let age ='14'
+console.log(age)
+const myname = 'Kira'
+ console.log(myname);
+
+ let isStudent ='true or false'
+console.log(isStudent)
+
+
+let myString  ='Життя не має обмежень окрім тих які ми створюємо собі самі'
+console.log(myString )
+
+
+
+let myNumber = 100;
+myNumber = myNumber + 10;
+
+console.log( myNumber);
+
+let myNull = null;
+
+console.log( myNull);
+
+
+
+
+const userName = prompt("Будь ласка, введіть своє ім'я");
+
+
+if (userName) {
+    
+   alert("Вітаю, " + userName );
+}
+
+console.log(userName);
+
+
+const isComing = confirm("Підтвердіть дію");
+
+
+
+console.log(isComing);
