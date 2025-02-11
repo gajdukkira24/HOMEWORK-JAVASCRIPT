d24628f36830307a91a2605e04c311b822906689
Kira <gajdukkira232@gmail.com>
2025-02-11 19:22:59 +0200

    homework3

---
+++ js/homework3-strinng.js
@@ -0,0 +1,67 @@
+//  1. Вивести result в консоль. Перевірити тип змінної result= 5+5+’5’.
+const result= 5+5+`5`;
+console.log(result);
+
+
+
+//  2. Створіть змінну email з вашою електронною адресою. Напишіть скрипт,
+//  який перевіряє чи містить змінна email символ @ (за доп метода includes()  або indexOf()) та
+//  рахує загальну кількість символів. Результат виведіть в консоль.
+
+const email ="gajdukkira232@gmail.com"
+console.log(email .includes("@"));
+const emailCheck =email.length;
+console.log( emailCheck);
+
+
+// 3. Записати в окремих змінних кожне слово: My, name, is. Поєднати всі слова в змінну fullName.
+//  До змінної fullName додати ‘Viktor’. fullName вивести в консоль.(працюємо з конкатенацією)
+
+let word1 ="my";
+let word2 ="name";
+let word3 = "is"
+let fullName = word1 + " " + word2 + " " + word3;
+
+console.log(fullName +  "Victor");
+
+
+
+
+//  4. Створіть змінну userName з ім’ям користувача і змінну payment з сумою до оплати. 
+// За допомогою шаблонного рядка виведіть на екран через alert повідомлення на прикладі цього: «Дякуємо, Олександро! До сплати 300 гривень»
+const userName = "Катерина";
+const payment ="! До сплати 700 гривень";
+const thanks = "Дякуємо," ;
+const messageThanks = alert(thanks + " " + userName + " " + payment );
+console.log(messageThanks );
+
+
+
+
+
+
+
+// Додаткові для практики:
+// 5. Створити змінну, яка містить рядок з вашим ім'ям та прізвищем.
+//  Використовуючи метод .indexOf(), знайти і вивести в консоль 
+// позицію, на якій знаходиться пробіл між ім'ям та прізвищем.
+
+const myName ="Кіра  Гайдук";
+console.log(myName .indexOf(" "));
+
+
+
+
+// 6. Написати скрипт пошуку в рядку. Дано:
+const blacklistedWord1 = 'спам';
+const blacklistedWord2 = 'розпродаж';
+const string1 = 'Привіт, я Ілон Маск, це не спам, пропоную тобі мільйон!';
+const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+const string3 = 'Рекламна кампанія #jsIsAwesome';
+
+const userWord = prompt("Будь ласка, введіть слово для пошуку:");
+
+if (userWord) {
+  const lowerUserWord = userWord.toLowerCase();
+
+}
